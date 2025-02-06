7506cc5ad0fd8258eb04a8f94c677ea2de72bdc3
macmillianzungene <macmillianzungene@gmail.com>
2025-02-07 01:20:40 +0200

    created and updatedthe api.js file

---
+++ frontend/src/api.js
@@ -0,0 +1,25 @@
+// src/api.js
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:5000';  // Flask backend URL
+
+// Register user
+export const registerUser = (userData) => {
+  return axios.post(`${API_URL}/register`, userData);
+};
+
+// Login user
+export const loginUser = (userData) => {
+  return axios.post(`${API_URL}/login`, userData);
+};
+
+// Add expense
+export const addExpense = (expenseData) => {
+  return axios.post(`${API_URL}/add_expense`, expenseData);
+};
+
+// Set goal
+export const setGoal = (goalData) => {
+  return axios.post(`${API_URL}/set_goal`, goalData);
+};
+
